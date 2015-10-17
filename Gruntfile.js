ac238ae0202f0aee4b5f5af1a2d5f0c502935de7
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2015-10-17 18:02:14 +0300

    This commit will begin forking from petuomin/marc-record-serializers.
    Add necessary files and modify package.json.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,65 +1,44 @@
-'use strict';
+module.exports = function(grunt)
+{
 
-module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-mocha-istanbul');
 
-	// Project configuration.
-	grunt.initConfig({
-		jshint: {
-			options: {
-				jshintrc: '.jshintrc'
-			},
-			gruntfile: {
-				src: 'Gruntfile.js'
-			},
-			lib: {
-				src: ['lib/**/*.js']
-			},
-		 
-		},
+    grunt.initConfig({
 
-		mochaTest: {
-			test: {
-				options: {
-					reporter: 'spec'
-				},
-				src: [ 'test/**/*.spec.js' ]
-			}
-		},
+	jshint: {
+	    options: {
+		jshintrc: true
+	    },
+	    src: '.'
+	},
+	mochaTest: {
+	    test: {
+		src: [ 'test/*.spec.js' ]
+	    }
+	},
+	mocha_istanbul: {
+	    src: 'test',
+	    options: {
+		check: {
+		    lines: 80,
+		    statements: 80,
+		    functions: 80,
+		    branches: 80
+		}
+	    }
+	}
 
-		mocha_istanbul: {
-           
-            coveralls: {
-                src: ['test'],
-                options: {
-                    coverage: true,
-                    check: {
-                        lines: 90,
-                        statements: 90,
-                        branches: 80,
-                        functions: 90
-                    }
-                }
-            }
-        },
+    });
 
-        watch: {
-        	test: {
-        		files: ['**/*.js'],
-        		tasks: ['mochaTest']
-        	}
-        }
+    grunt.registerTask('lint', ['jshint']);
+    grunt.registerTask('test', ['mochaTest']);
+    grunt.registerTask('coverage', ['mocha_istanbul']);
+    grunt.registerTask('default', [
+	'lint',
+	'test',
+	'coverage'
+    ]);
 
-
-	});
-
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-mocha-test');
-	grunt.loadNpmTasks('grunt-mocha-istanbul');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-
-	grunt.registerTask('lint', ['jshint']);
-	grunt.registerTask('test', ['mochaTest']);
-	grunt.registerTask('coverage', ['mocha_istanbul']);
-
-	grunt.registerTask('default', ['lint', 'test', 'coverage']);
 };
---
+++ README.md
@@ -0,0 +1,30 @@
+# MARC Record converters
+
+Convert MARC records between different formats
+
+TODO
+
+## Usage
+
+### Installation
+
+TODO
+
+### Configuration
+
+TODO
+
+### Running
+
+TODO
+
+## Examples
+
+TODO
+
+## License and copyright
+
+Copyright (c) 2015 University Of Helsinki (The National Library Of Finland)
+Copyright (c) 2015 Pasi Tuominen
+
+This project's source code is licensed under the terms of **ISC License**.
